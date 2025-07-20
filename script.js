9103ef47585183fd451b366690ae28b436623e5a
Ashadow698 <lucasarlt@icloud.com>
2025-07-20 12:18:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function generateAdjective() {
-    const adjectives = ['awesome', 'terrible', 'okay'];
+    const adjectives = [
+        'awesome', 'terrible', 'okay', 'slow', 'gay', 'lazy', 'quick', 'happy', 
+        'sad', 'energetic', 'intelligent', 'funny', 'boring', 'friendly', 'grumpy',
+        'amazing', 'awkward', 'shy', 'bold', 'confident', 'lovable', 'outgoing', 
+        'smart', 'adventurous', 'clumsy', 'serious', 'playful'
+    ];
+    
     const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-
+    
     const messageDiv = document.getElementById('message');
     messageDiv.textContent = `Gagy is ${randomAdjective}`;
 }
